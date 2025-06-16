a7486d2bbcda22e90d33a901431349144e95bcf4
Vasu-0846 <bariyavasu46@gmail.com>
2025-06-16 13:24:18 +0530

    tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <h1 className='text-teal-500 text-5xl font-light font-mono	text-center'>Hii This React Project Using Vite</h1>
+    </>
+  )
+}
+
+export default App
